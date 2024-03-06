f9c85cea7e7ce51110a2c27fc38238ef6fed1d79
mango606 <mango606@email.com>
2024-03-06 12:42:19 +0900

    Feat: 기본 레이아웃 및 라우팅 설정 추가
    - Header 컴포넌트 추가하여 애플리케이션의 상단에 공통 헤더 구현
    - HomePage 컴포넌트 생성하여 초기 홈 페이지 구성
    - react-router-dom을 활용한 기본 라우팅 설정 구현
    - App 컴포넌트에서 Header와 HomePage를 라우팅에 포함

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/common/Header';
+
+function HomePage() {
+    return (
+        <div>
+            <Header />
+            <h1>Knujoon</h1>
+        </div>
+    );
+}
+
+export default HomePage;
